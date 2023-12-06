26210892e19f41848641d6cb0cbab48f089ad8b9
Snnat19 <jnatsanchezrr@gmail.com>
2023-12-06 17:51:49 -0500

    feat(frontend): add controllers

--- ApiRestBilling2/ApiRestBilling2/Data/ApplicationDbContext.cs
+++ ApiRestBilling2/ApiRestBilling2/Data/ApplicationDbContext.cs
@@ -10,5 +10,10 @@ namespace ApiRestBilling.Data
         {
 
         }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
